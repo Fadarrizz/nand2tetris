e4c092135515407fa0ea96d34a74ad047b58d48b
Fadarrizz <auk.geerts@gmail.com>
2020-05-22 09:34:47 +0200

    Finish Mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,23 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0
+
+(LOOP)
+    @R1
+    D=M		// Store R1
+    @END
+    D;JEQ	// Goto end if R1 == 0
+    @R0
+    D=M		// Store R0
+    @R2
+    M=D+M	// R2 = R2 + R0
+    @R1
+    M=M-1	// R1--
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
